9503dd9ecfc56668d62feada5aa34473c578f363
Ale Roses <ale.vrs@outlook.com>
2025-10-17 11:57:34 -0500

    29.7 Testing authSlice states and actions

--- tests/store/auth/authSlice.test.js
+++ tests/store/auth/authSlice.test.js
@@ -1,8 +1,75 @@
-import { authSlice } from "../../../src/store/auth/authSlice";
-import { initialState } from "../../fixtures/authStates";
+import {
+  authSlice,
+  clearErrorMessage,
+  onLogin,
+  onLogout,
+} from "../../../src/store/auth/authSlice";
+import {
+  authenticatedState,
+  initialState,
+} from "../../fixtures/authStates";
+import { testUserCredentials } from "../../fixtures/testUser";
 
 describe("Test in authSlice", () => {
   test("It must return to the initial state.", () => {
     expect(authSlice.getInitialState()).toEqual(initialState);
+  });
+
+  test("It must log in.", () => {
+    const state = authSlice.reducer(
+      initialState,
+      onLogin(testUserCredentials)
+    );
+    // console.log(state);
+
+    expect(state).toEqual({
+      status: "authenticated",
+      user: testUserCredentials,
+      errorMessage: undefined,
+    });
+  });
+
+  test("It must log out.", () => {
+    const state = authSlice.reducer(
+      authenticatedState,
+      onLogout()
+    );
+
+    expect(state).toEqual({
+      status: "not-authenticated",
+      user: {},
+      errorMessage: undefined,
+    });
+  });
+
+  test("It must log out.", () => {
+    const errorMessage = "Invalid credentials.";
+    const state = authSlice.reducer(
+      authenticatedState,
+      onLogout(errorMessage)
+    );
+    // console.log(state);
+
+    expect(state).toEqual({
+      status: "not-authenticated",
+      user: {},
+      errorMessage: errorMessage,
+    });
+  });
+
+  test("It must clear the error message.", () => {
+    const errorMessage = "Invalid credentials.";
+    const state = authSlice.reducer(
+      authenticatedState,
+      onLogout(errorMessage)
+    );
+    // console.log(state);
+
+    const newState = authSlice.reducer(
+      state,
+      clearErrorMessage()
+    );
+
+    expect(newState.errorMessage).toBe(undefined);
   });
 });
