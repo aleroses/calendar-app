432983b6cc45e6513dab97e9efd6921433263dca
Ale Roses <ale.vrs@outlook.com>
2025-10-20 14:15:22 -0500

    29.16 startLogin must login correctly

--- tests/hooks/useAuthStore.test.js
+++ tests/hooks/useAuthStore.test.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "../../src/store/auth/authSlice";
-import { initialState } from "../fixtures/authStates";
-import { renderHook } from "@testing-library/react";
+import {
+  initialState,
+  notAuthenticatedState,
+} from "../fixtures/authStates";
+import { act, renderHook } from "@testing-library/react";
 import { useAuthStore } from "../../src/hooks/useAuthStore";
 import { Provider } from "react-redux";
+import { testUserCredentials } from "../fixtures/testUser";
 
 const getMockStore = (initialState) => {
   return configureStore({
@@ -35,5 +39,41 @@ describe("Tests in useAuthStore", () => {
       startLogout: expect.any(Function),
       startRegister: expect.any(Function),
     });
+  });
+
+  test("startLogin should login correctly.", async () => {
+    localStorage.clear();
+
+    const mockStore = getMockStore({
+      ...notAuthenticatedState,
+    });
+    const { result } = renderHook(() => useAuthStore(), {
+      wrapper: ({ children }) => (
+        <Provider store={mockStore}>{children}</Provider>
+      ),
+    });
+
+    await act(async () => {
+      await result.current.startLogin(testUserCredentials);
+    });
+
+    const { errorMessage, status, user } = result.current;
+    // console.log(result.current);
+
+    expect({ errorMessage, status, user }).toEqual({
+      errorMessage: undefined,
+      status: "authenticated",
+      user: {
+        name: "Test User",
+        uid: "68f0eab1486246c8bcd69137",
+      },
+    });
+
+    expect(localStorage.getItem("token")).toEqual(
+      expect.any(String)
+    );
+    expect(localStorage.getItem("token-init-date")).toEqual(
+      expect.any(String)
+    );
   });
 });
